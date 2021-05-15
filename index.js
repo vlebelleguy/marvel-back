b12b85dfd1eeab9fe92e2315e5c3ee96d786aaf5
vlebelleguy <80490764+vlebelleguy@users.noreply.github.com>
2021-05-15 20:26:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
+app.get("/characters", async (req, res) => {
+   try {
+      let limit = 100;
+      if (req.query.limit) {
+         limit = Number(req.query.limit);
+      }
+
+      let skip = 0;
+      if (req.query.skip) {
+         skip = Number(req.query.skip);
+      }
+
+      let name = "";
+      if (req.query.name) {
+         name = req.query.name;
+      }
+
+      const response = await axios.get(
+         `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&name=${name}`
+      );
+
+      res.status(200).json(response.data);
+   } catch (error) {
+      console.log(error);
+   }
+});
+
 app.get("/comics", async (req, res) => {
    try {
       let limit = 100;
